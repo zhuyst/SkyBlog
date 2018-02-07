66daa519deca5fdfed7f1da4499e221a881e93d3
zhuyst <13553371651@139.com>
2018-02-07 15:22:36 +0800

    优化

--- server_api/src/main/java/indi/zhuyst/security/service/impl/SecurityServiceImpl.java
+++ server_api/src/main/java/indi/zhuyst/security/service/impl/SecurityServiceImpl.java
@@ -27,12 +27,12 @@ import java.util.Date;
 public class SecurityServiceImpl extends BaseService implements SecurityService{
 
     /**
-     * username在Claims中的name
+     * {@link SecurityUser#username}在Claims中的name
      */
     private static final String CLAIM_USERNAME = "username";
 
     /**
-     * role在Claims中的name
+     * {@link SecurityUser#role}在Claims中的name
      */
     private static final String CLAIM_ROLE = "role";
 
@@ -64,12 +64,19 @@ public class SecurityServiceImpl extends BaseService implements SecurityService{
         Date nowDate = new Date();
         Date expireDate = new Date(nowDate.getTime() + expire * 1000);
 
+        // 构建JWT
         String token = Jwts.builder()
+
+                // 将User的标识信息放入JWT中
                 .setSubject(String.valueOf(user.getId()))
                 .claim(CLAIM_USERNAME,user.getUsername())
                 .claim(CLAIM_ROLE,user.getRole())
+
+                // 设置过期时间
                 .setIssuedAt(nowDate)
                 .setExpiration(expireDate)
+
+                // 设置加密方式
                 .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
 
@@ -89,6 +96,7 @@ public class SecurityServiceImpl extends BaseService implements SecurityService{
     public SecurityUser getUserByToken(String token) {
         Claims claims = this.getClaimByToken(token);
 
+        // 从JWT中读取信息，构建User
         User user = new User();
         user.setId(Integer.valueOf(claims.getSubject()));
         user.setUsername(claims.get(CLAIM_USERNAME,String.class));
@@ -98,24 +106,42 @@ public class SecurityServiceImpl extends BaseService implements SecurityService{
     }
 
     @Override
-    public Claims getClaimByToken(String token) {
-        if(this.isTokenValid(token)){
-            throw new AccessDeniedException("token失效，请重新登录");
-        }
+    public boolean isTokenValid(String token) {
+        Claims claims = this.getClaimByToken(token);
+        return isTokenValid(claims);
+    }
 
+    /**
+     * 获取JWT的Claims
+     * @param token JWT
+     * @return Claims
+     */
+    private Claims getClaimByToken(String token) {
         try {
-            return Jwts.parser()
+            Claims claims = Jwts.parser()
                     .setSigningKey(secret)
                     .parseClaimsJws(token)
                     .getBody();
+
+            // 如果Token不可用，抛出异常
+            if(this.isTokenValid(claims)){
+                throw new AccessDeniedException("token失效，请重新登录");
+            }
+
+            return claims;
         }catch (Exception e){
+
+            // Token转换异常，表示不合法，抛出异常
             throw new AccessDeniedException("invalid token");
         }
     }
 
-    @Override
-    public boolean isTokenValid(String token) {
-        Claims claims = this.getClaimByToken(token);
+    /**
+     * 通过Claims判断Token是否合法
+     * @param claims Claims
+     * @return 是否合法
+     */
+    private boolean isTokenValid(Claims claims){
         return isTokenExpired(claims.getExpiration());
     }
 
