5583870c729bab89627c289464edaf5c30e71983
zhuyst <13553371651@139.com>
2018-02-07 17:56:33 +0800

    修复BUG

--- server_api/src/main/java/indi/zhuyst/security/controller/AuthController.java
+++ server_api/src/main/java/indi/zhuyst/security/controller/AuthController.java
@@ -6,6 +6,8 @@ import indi.zhuyst.common.pojo.R;
 import indi.zhuyst.security.pojo.AccessToken;
 import indi.zhuyst.security.pojo.SecurityUser;
 import indi.zhuyst.security.service.SecurityService;
+import indi.zhuyst.skyblog.entity.User;
+import indi.zhuyst.skyblog.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,9 @@ public class AuthController extends BaseController{
 
     @Autowired
     private SecurityService securityService;
+
+    @Autowired
+    private UserService userService;
 
     /**
      * 欺骗Swagger的扫描，正式的登陆授权由Spring Security完成
@@ -56,8 +61,10 @@ public class AuthController extends BaseController{
     @ApiOperation("通过老Token换取新Token")
     @RequestMapping(value = "/refresh",method = RequestMethod.POST)
     public R<AccessToken> refresh(String token){
-        SecurityUser user = securityService.getUserByToken(token);
-        AccessToken accessToken = securityService.generateToken(user);
+        Integer userId = securityService.getIDByToken(token);
+        User user = userService.getByID(userId);
+
+        AccessToken accessToken = securityService.generateToken(new SecurityUser(user));
         return R.ok(accessToken);
     }
 }
