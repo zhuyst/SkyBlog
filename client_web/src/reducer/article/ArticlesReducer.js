39254a8672bc5618a0d84f3c43c90986272e8334
zhuyst <13553371651@139.com>
2018-02-10 18:45:32 +0800

    优化 更多文章 的性能

--- client_web/src/reducer/article/ArticlesReducer.js
+++ client_web/src/reducer/article/ArticlesReducer.js
@@ -11,7 +11,32 @@ const ArticlesReducer = (state = initialState,action) => {
     switch (action.type){
         case LIST_ARTICLES_RESPONSE:
             const page = action.page;
-            const newList = state.list.concat(page.list);
+
+            const stateList = state.list;
+            const resultList = page.list;
+            let newList;
+            if(stateList.length < page.page_size){
+                newList = stateList.concat(resultList);
+            }
+            else {
+                newList = stateList.concat([]);
+                for(let i = 0;i < resultList.length;i++){
+                    let isRepeat = false;
+
+                    for(let j = stateList.length - page.page_size;
+                        j < stateList.length;j++){
+                        console.log(i + "--" + j);
+                        if(resultList[i].id === stateList[j].id){
+                            isRepeat = true;
+                            break;
+                        }
+                    }
+
+                    if(!isRepeat){
+                        newList.push(resultList[i]);
+                    }
+                }
+            }
 
             return {
                 ...state,
