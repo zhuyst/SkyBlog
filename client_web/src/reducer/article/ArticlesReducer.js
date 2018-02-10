87171aaab0efad390b542e498fcaa43db738997b
zhuyst <13553371651@139.com>
2018-02-10 18:49:21 +0800

    注释

--- client_web/src/reducer/article/ArticlesReducer.js
+++ client_web/src/reducer/article/ArticlesReducer.js
@@ -15,6 +15,9 @@ const ArticlesReducer = (state = initialState,action) => {
             const stateList = state.list;
             const resultList = page.list;
             let newList;
+
+            // 由于有可能出现重复元素，破坏React遍历唯一性
+            // 所以要进行数组合并查重
             if(stateList.length < page.page_size){
                 newList = stateList.concat(resultList);
             }
@@ -23,6 +26,8 @@ const ArticlesReducer = (state = initialState,action) => {
                 for(let i = 0;i < resultList.length;i++){
                     let isRepeat = false;
 
+                    // 由于数组只有可能在后面page_size范围内出现重复元素
+                    // 所以只需要扫描这个范围进行去重即可
                     for(let j = stateList.length - page.page_size;
                         j < stateList.length;j++){
                         console.log(i + "--" + j);
