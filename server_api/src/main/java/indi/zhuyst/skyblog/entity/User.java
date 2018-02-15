a203e6045b7f8566b0c883677e346e8f6185dd89
zhuyst <13553371651@139.com>
2018-02-15 13:17:29 +0800

    修改User

--- server_api/src/main/java/indi/zhuyst/skyblog/entity/User.java
+++ server_api/src/main/java/indi/zhuyst/skyblog/entity/User.java
@@ -3,9 +3,7 @@ package indi.zhuyst.skyblog.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import indi.zhuyst.common.entity.BaseEntity;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Pattern;
@@ -14,6 +12,8 @@ import javax.validation.constraints.Pattern;
  * 用户
  * @author zhuyst
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 @NoArgsConstructor
 public class User extends BaseEntity {
 
@@ -25,8 +25,6 @@ public class User extends BaseEntity {
     @ApiModelProperty("用户名")
     @Length(min = 4, max = 10, message = "用户名长度应在{min}与{max}之间")
     @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "用户名应为字母或数字的组合")
-    @Getter
-    @Setter
     private String username;
 
     /**
@@ -35,7 +33,6 @@ public class User extends BaseEntity {
     @ApiModelProperty("密码")
     @Length(min = 6, max = 20, message = "密码长度应在{min}与{max}之间")
     @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "密码应为字母或数字的组合")
-    @Setter
     private String password;
 
     /**
@@ -43,22 +40,12 @@ public class User extends BaseEntity {
      */
     @ApiModelProperty("昵称")
     @Length(min = 2,max = 8, message = "昵称长度应在{min}与{max}之间")
-    @Getter
-    @Setter
     private String nickname;
 
     /**
      * 角色ID
      * @see indi.zhuyst.security.enums.RoleEnum
      */
-    @ApiModelProperty(hidden = true)
-    @JsonIgnore
-    @Getter
-    @Setter
+    @ApiModelProperty("角色ID")
     private Integer role;
-
-    @JsonIgnore
-    public String getPassword() {
-        return password;
-    }
 }
