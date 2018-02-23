80acfe261b2f7fd7c1aac71ec485ceb2f503cd2c
zhuyst <13553371651@139.com>
2018-02-23 16:35:59 +0800

    留言板新增个人信息板

--- client_web/src/component/msgboard/Index.js
+++ client_web/src/component/msgboard/Index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import {Col, Row} from "react-bootstrap";
+import {Col, Panel, Row} from "react-bootstrap";
 
 import MsgList from "./MsgList";
 import MsgSender from './MsgSender'
 
 import '../../static/css/msgboard/msgboard.css'
+import UserInfoTable from "../common/UserInfoTable";
+import {connect} from "react-redux";
 
 class Index extends React.Component{
     componentWillMount(){
@@ -12,17 +14,44 @@ class Index extends React.Component{
     }
 
     render(){
+        const login = this.props.login;
+
         return (
             <Row className="msg_board_main">
-                <Col mdOffset={1} md={6}>
+                <Col mdOffset={1} md={6} sm={12}>
                     <MsgList/>
                 </Col>
-                <Col md={4}>
-                    <MsgSender/>
+                <Col md={4} sm={12}>
+                    <Col lgOffset={3} lg={6} mdOffset={2} md={8} sm={12}>
+                        <Panel bsStyle="primary">
+                            <Panel.Heading>
+                                <Panel.Title componentClass="h3">
+                                    当前登录用户信息
+                                </Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body>
+                                <UserInfoTable user={login.user}/>
+                            </Panel.Body>
+                        </Panel>
+                    </Col>
+                    <Col md={12} sm={12}>
+                        <MsgSender/>
+                    </Col>
                 </Col>
             </Row>
         )
     }
 }
 
-export default Index
+const mapStateToProps = state => {
+    return {
+        login : state.login
+    }
+};
+
+const IndexContainer = connect(
+    mapStateToProps,
+    null
+)(Index);
+
+export default IndexContainer
