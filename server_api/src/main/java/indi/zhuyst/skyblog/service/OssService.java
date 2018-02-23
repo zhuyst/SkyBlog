0399af5ecbf0c4e704d53fb4652c63640b93e80f
zhuyst <13553371651@139.com>
2018-02-24 00:50:59 +0800

    OSS上传文件功能

--- server_api/src/main/java/indi/zhuyst/skyblog/service/OssService.java
+++ server_api/src/main/java/indi/zhuyst/skyblog/service/OssService.java
@@ -1,4 +1,9 @@
 package indi.zhuyst.skyblog.service;
 
+import indi.zhuyst.skyblog.pojo.OssFile;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface OssService {
+
+    OssFile upload(MultipartFile file);
 }
--- server_api/src/main/java/indi/zhuyst/skyblog/setting/OssSettings.java
+++ server_api/src/main/java/indi/zhuyst/skyblog/setting/OssSettings.java
@@ -2,12 +2,14 @@ package indi.zhuyst.skyblog.setting;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * 阿里云OSS服务相关配置
  * @author zhuyst
  */
 @Data
+@Component
 @ConfigurationProperties(prefix = "skyblog.oss")
 public class OssSettings {
 
@@ -15,6 +17,11 @@ public class OssSettings {
      * Bucket的EndPoint
      */
     private String endPoint;
+
+    /**
+     * 访问文件时的域名
+     */
+    private String accessDomain;
 
     /**
      * 阿里云账号的AccessKeyId
@@ -25,4 +32,9 @@ public class OssSettings {
      * 阿里云账号的AccessKeySecret
      */
     private String accessKeySecret;
+
+    /**
+     * 上传的Bucket
+     */
+    private String bucketName;
 }
