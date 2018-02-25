6abb007cd53660be797bff88d3d9fe6139e273e1
zhuyst <13553371651@139.com>
2018-02-25 13:50:57 +0800

    首页图片更新

--- client_web/src/component/home/NavImage.js
+++ client_web/src/component/home/NavImage.js
@@ -1,34 +1,25 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
+import {Button, Carousel} from 'react-bootstrap'
 
-import image1 from '../../static/images/1.jpg'
-import image2 from '../../static/images/2.jpg'
-import image3 from '../../static/images/3.jpg'
+import home from '../../static/images/home.jpg'
 
 class NavImage extends React.Component{
     render(){
         return (
-            <Carousel className="navImage">
-                <Carousel.Item className="navImage">
-                    <img height={300} alt="" src={image1}/>
-                    <Carousel.Caption>
-                        <h3>第一张图片</h3>
-                        <p>第一张图片的介绍</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item className="navImage">
-                    <img height={300} alt="" src={image2}/>
-                    <Carousel.Caption>
-                        <h3>第二张图片</h3>
-                        <p>第二张图片的介绍</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item className="navImage">
-                    <img height={300} alt="" src={image3}/>
-                    <Carousel.Caption>
-                        <h3>第三张图片</h3>
-                        <p>第三张图片的介绍</p>
-                    </Carousel.Caption>
+            <Carousel className="nav_image">
+                <Carousel.Item className="nav_image">
+                    <div className="nav_image_text">
+                        <h1>Sky&nbsp;Blog</h1>
+                        <div className="hidden-xs">
+                            <hr/>
+                            <h3>一个将阅读与管理合一的轻量级博客</h3>
+                        </div>
+                    </div>
+                    <div className="nav_image_button_group">
+                        <Button bsSize="large">查看SkyBlog源码</Button>
+                        <Button bsSize="large">查看作者Github</Button>
+                    </div>
+                    <img height={300} alt="" src={home}/>
                 </Carousel.Item>
             </Carousel>
         )
