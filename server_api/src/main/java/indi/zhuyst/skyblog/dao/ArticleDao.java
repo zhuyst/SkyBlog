20c1aae14ddb615510cd50d1884b9a939ed6ef99
zhuyst <13553371651@139.com>
2018-02-26 18:05:17 +0800

    DAO注释

--- server_api/src/main/java/indi/zhuyst/skyblog/dao/ArticleDao.java
+++ server_api/src/main/java/indi/zhuyst/skyblog/dao/ArticleDao.java
@@ -7,11 +7,26 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * 文章DAO
+ * @author zhuyst
+ */
 @Repository
 public interface ArticleDao extends BaseDao<Article> {
 
+    /**
+     * 在{@link #select(Object)}基础上可以排除IDs
+     * @param article {@link #select(Object)}一致
+     * @param ids 要排除的Ids
+     * @return 文章对象列表
+     */
     List<Article> selectWithoutIDs(@Param("article") Article article,
                                    @Param("list") List<Integer> ids);
 
+    /**
+     * 查询除了{@link Article#content}以外的分类文章
+     * @param classifyId 分类ID
+     * @return 文章对象列表
+     */
     List<Article> selectBaseInfoByClassify(int classifyId);
 }
