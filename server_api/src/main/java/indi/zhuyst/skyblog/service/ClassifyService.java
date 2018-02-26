b85bdf74d7cccb8612bc969516581112b67fbc70
zhuyst <13553371651@139.com>
2018-02-26 18:41:55 +0800

    Classify接口优化

--- server_api/src/main/java/indi/zhuyst/skyblog/service/ClassifyService.java
+++ server_api/src/main/java/indi/zhuyst/skyblog/service/ClassifyService.java
@@ -1,20 +1,20 @@
 package indi.zhuyst.skyblog.service;
 
+import indi.zhuyst.common.service.BaseCrudService;
 import indi.zhuyst.common.service.CacheableService;
 import indi.zhuyst.skyblog.entity.Classify;
 import indi.zhuyst.skyblog.pojo.ClassifyDTO;
 
 import java.util.List;
 
-public interface ClassifyService extends CacheableService{
+public interface ClassifyService extends BaseCrudService<Classify>,
+        CacheableService{
 
     int NOT_CLASSIFY_KEY = 1;
 
     String CACHE_OBJECT = CACHE_PREFIX + "classify";
 
     String CACHE_LIST = CACHE_PREFIX + "classify_list";
-
-    Classify getById(int id);
 
     ClassifyDTO getClassifyDTO(int id);
 
@@ -23,7 +23,5 @@ public interface ClassifyService extends CacheableService{
     List<ClassifyDTO> listClassify();
 
     ClassifyDTO saveClassify(Classify classify);
-
-    boolean deleteClassify(int id);
 
 }
