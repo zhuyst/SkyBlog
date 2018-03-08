63179faffb5b82efae545be212b545c5383e2d7a
zhuyst <13553371651@139.com>
2018-03-08 15:47:27 +0800

    添加表 访问日志

---
+++ server_api/src/main/java/indi/zhuyst/skyblog/dao/AccessLogDao.java
@@ -0,0 +1,11 @@
+package indi.zhuyst.skyblog.dao;
+
+import indi.zhuyst.common.dao.BaseDao;
+import indi.zhuyst.skyblog.entity.AccessLogDO;
+
+/**
+ * 访问日志DAO
+ * @author zhuyst
+ */
+public interface AccessLogDao extends BaseDao<AccessLogDO> {
+}
---
+++ server_api/src/main/java/indi/zhuyst/skyblog/entity/AccessLogDO.java
@@ -0,0 +1,41 @@
+package indi.zhuyst.skyblog.entity;
+
+import indi.zhuyst.common.entity.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ * 访问日志
+ * @author zhuyst
+ */
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "access_log")
+@Data
+public class AccessLogDO extends BaseEntity{
+
+    private static final long serialVersionUID = -6902942334575584596L;
+
+    /**
+     * 访问IP
+     */
+    @ApiModelProperty("访问ID")
+    private String ip;
+
+    /**
+     * 访问用户ID
+     */
+    @Column(name = "user_id")
+    @ApiModelProperty("访问用户ID")
+    private Integer userId;
+
+    /**
+     * 访问日期
+     */
+    @Column(name = "access_date")
+    @ApiModelProperty("访问日期")
+    private Date accessDate;
+}
