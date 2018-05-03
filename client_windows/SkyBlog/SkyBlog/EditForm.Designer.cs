b51a41f280c7ca07f95cc326bc7896658b539000
zhuyst <13553371651@139.com>
2018-05-03 22:17:02 +0800

    创建EditForm

---
+++ client_windows/SkyBlog/SkyBlog/EditForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SkyBlog
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditForm));
+            this.dSkinTableLayoutPanel1 = new DSkin.Controls.DSkinTableLayoutPanel();
+            this.TitleTextBox = new DSkin.Controls.DSkinTextBox();
+            this.dSkinTableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dSkinTableLayoutPanel1
+            // 
+            this.dSkinTableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.dSkinTableLayoutPanel1.BitmapCache = false;
+            this.dSkinTableLayoutPanel1.ColumnCount = 2;
+            this.dSkinTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.77273F));
+            this.dSkinTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.22727F));
+            this.dSkinTableLayoutPanel1.Controls.Add(this.TitleTextBox, 1, 0);
+            this.dSkinTableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dSkinTableLayoutPanel1.Location = new System.Drawing.Point(14, 50);
+            this.dSkinTableLayoutPanel1.Name = "dSkinTableLayoutPanel1";
+            this.dSkinTableLayoutPanel1.RightBottom = ((System.Drawing.Image)(resources.GetObject("dSkinTableLayoutPanel1.RightBottom")));
+            this.dSkinTableLayoutPanel1.RowCount = 4;
+            this.dSkinTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.628319F));
+            this.dSkinTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.37168F));
+            this.dSkinTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.dSkinTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.dSkinTableLayoutPanel1.Size = new System.Drawing.Size(968, 493);
+            this.dSkinTableLayoutPanel1.TabIndex = 0;
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.BitmapCache = false;
+            this.TitleTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TitleTextBox.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TitleTextBox.Location = new System.Drawing.Point(146, 3);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(819, 34);
+            this.TitleTextBox.TabIndex = 1;
+            this.TitleTextBox.TransparencyKey = System.Drawing.Color.Empty;
+            this.TitleTextBox.WaterFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TitleTextBox.WaterText = "";
+            this.TitleTextBox.WaterTextOffset = new System.Drawing.Point(0, 0);
+            // 
+            // EditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(251)))), ((int)(((byte)(251)))));
+            this.CanResize = false;
+            this.CaptionBackColors = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(237)))), ((int)(((byte)(247)))))};
+            this.CaptionCenter = true;
+            this.CaptionFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.CaptionHeight = 35;
+            this.ClientSize = new System.Drawing.Size(996, 557);
+            this.Controls.Add(this.dSkinTableLayoutPanel1);
+            this.DoubleClickMaximized = false;
+            this.DrawIcon = false;
+            this.EnabledDWM = true;
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.InheritTheme = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "EditForm";
+            this.Opacity = 0.95D;
+            this.Padding = new System.Windows.Forms.Padding(10, 15, 10, 10);
+            this.Text = "EditForm";
+            this.Load += new System.EventHandler(this.EditForm_Load);
+            this.dSkinTableLayoutPanel1.ResumeLayout(false);
+            this.dSkinTableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DSkin.Controls.DSkinTableLayoutPanel dSkinTableLayoutPanel1;
+        private DSkin.Controls.DSkinLabel TitleLabel;
+        private DSkin.Controls.DSkinTextBox TitleTextBox;
+        private DSkin.Controls.DSkinLabel SubTitleLabel;
+    }
+}
