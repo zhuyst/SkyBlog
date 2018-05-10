3dac75cf17fd1dc8a0abbae84415f4f518487701
zhuyst <13553371651@139.com>
2018-05-10 21:47:39 +0800

    新增、修改文章接入

--- client_windows/SkyBlog/SkyBlog.Model/Base/Result.cs
+++ client_windows/SkyBlog/SkyBlog.Model/Base/Result.cs
@@ -2,8 +2,15 @@
 {
     public class Result
     {
+        public const int SuccessCode = 200; 
+
         public int Code { get; set; }
 
         public string Message { get; set; }
+
+        public bool IsSuccess()
+        {
+            return Code == SuccessCode;
+        }
     }
 }
