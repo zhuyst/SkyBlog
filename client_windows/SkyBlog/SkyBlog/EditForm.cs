dbde5218de17518eeabe7141d78db0e3fd2154a1
zhuyst <13553371651@139.com>
2018-05-11 15:28:26 +0800

    修复记住密码与自动登录的触发事件

--- client_windows/SkyBlog/SkyBlog/EditForm.cs
+++ client_windows/SkyBlog/SkyBlog/EditForm.cs
@@ -8,8 +8,15 @@ namespace SkyBlog
 {
     public partial class EditForm : DSkinForm
     {
+        /// <summary>
+        /// 成功时调用的Action
+        /// </summary>
         public Action SuccessAction { get; set; }
 
+        /// <summary>
+        /// 文章对象
+        /// 如果为null则为新增文章，否则为编辑文章
+        /// </summary>
         public Article Article { get; set; }
 
         private readonly ArticleApi _articleApi;
@@ -22,17 +29,105 @@ namespace SkyBlog
 
         private void EditForm_Load(object sender, EventArgs e)
         {
+            // 如果Article为null，则是新增文章
             if (Article == null)
             {
                 Text = @"新增文章 - SkyBlog";
                 return;
             }
 
+            // 否则是编辑文章，将文章信息放入各个TextBox
             Text = $@"编辑文章 - {Article.Title}";
             TitleTextBox.Text = Article.Title;
             SubTitleTextBox.Text = Article.SubTitle;
+
+            // TextBox的换行符为\r\n，需要转换
             var content = Article.Content.Replace("\n", "\r\n");
             ContentTextBox.Text = content;
+        }
+
+        /// <summary>
+        /// 从TextBox获取文章对象
+        /// </summary>
+        /// <returns>获取的文章对象</returns>
+        private Article GetArticle()
+        {
+            return new Article()
+            {
+                Title = TitleTextBox.Text,
+                SubTitle = SubTitleTextBox.Text,
+                Content = ContentTextBox.Text
+            };
+        }
+
+        /// <summary>
+        /// 保存文章
+        /// </summary>
+        /// <returns>是否保存成功</returns>
+        private bool SaveArticle()
+        {
+            bool success;
+
+            // 如果Article为null，则是新增文章
+            if (Article == null)
+            {
+                var result = _articleApi.Insert(GetArticle());
+                success = result.IsSuccess();
+                DSkinMessageBox.Show(success ? "新增文章成功" : result.Message, "新增文章");
+            }
+
+            // 否则是编辑文章
+            else
+            {
+                var result = _articleApi.Update(GetArticle());
+                success = result.IsSuccess();
+                DSkinMessageBox.Show(success ? "修改文章成功" : result.Message, "修改文章");
+            }
+
+            // 成功时调用
+            if (success)
+            {
+                SuccessAction?.Invoke();
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// 表单验证
+        /// </summary>
+        /// <returns>表单验证是否通过</returns>
+        private bool CheckArticle()
+        {
+            if (!CheckTextBox(TitleTextBox))
+            {
+                DSkinMessageBox.Show("文章标题不能为空", "不能为空");
+                return false;
+            }
+
+            if (!CheckTextBox(SubTitleTextBox))
+            {
+                DSkinMessageBox.Show("文章副标题不能为空", "不能为空");
+                return false;
+            }
+
+            if (!CheckTextBox(ContentTextBox))
+            {
+                DSkinMessageBox.Show("文章内容不能为空", "不能为空");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 检查TextBox是否为空
+        /// </summary>
+        /// <param name="textBox">要检查的TextBox</param>
+        /// <returns>是否为空</returns>
+        private static bool CheckTextBox(TextBox textBox)
+        {
+            return textBox.Text.Trim() != string.Empty;
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
@@ -64,67 +159,6 @@ namespace SkyBlog
                 Close();
             }
         }
-
-        private Article GetArticle()
-        {
-            return new Article()
-            {
-                Title = TitleTextBox.Text,
-                SubTitle = SubTitleTextBox.Text,
-                Content = ContentTextBox.Text
-            };
-        }
-
-        private bool SaveArticle()
-        {
-            bool success;
-            if (Article == null)
-            {
-                var result = _articleApi.Insert(GetArticle());
-                success = result.IsSuccess();
-                DSkinMessageBox.Show(success ? "新增文章成功" : result.Message, "新增文章");
-            }
-            else
-            {
-                var result = _articleApi.Update(GetArticle());
-                success = result.IsSuccess();
-                DSkinMessageBox.Show(success ? "修改文章成功" : result.Message, "修改文章");
-            }
-
-            if (success)
-            {
-                SuccessAction?.Invoke();
-            }
-
-            return success;
-        }
-
-        private bool CheckArticle()
-        {
-            if (!CheckTextBox(TitleTextBox))
-            {
-                DSkinMessageBox.Show("文章标题不能为空", "不能为空");
-                return false;
-            }
-
-            if (!CheckTextBox(SubTitleTextBox))
-            {
-                DSkinMessageBox.Show("文章副标题不能为空", "不能为空");
-                return false;
-            }
-
-            if (!CheckTextBox(ContentTextBox))
-            {
-                DSkinMessageBox.Show("文章内容不能为空", "不能为空");
-                return false;
-            }
-
-            return true;
-        }
-
-        private static bool CheckTextBox(TextBox textBox)
-        {
-            return textBox.Text.Trim() != string.Empty;
-        }
+        
     }
 }
--- client_windows/SkyBlog/SkyBlog/LoginForm.cs
+++ client_windows/SkyBlog/SkyBlog/LoginForm.cs
@@ -9,6 +9,9 @@ namespace SkyBlog
 {
     public partial class LoginForm : DSkinForm
     {
+        /// <summary>
+        /// 登陆成功时执行的Action
+        /// </summary>
         public Action<User> LoginAction { get; set; }
 
         private readonly StorageService _storageService;
@@ -23,8 +26,76 @@ namespace SkyBlog
             _authApi = AuthApi.GetInstance();
         }
 
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // 读取用户信息放入TextBox
+            var userStorage = _storageService.GetUserStorage();
+            UsernameTextBox.Text = userStorage.Username;
+            PasswordTextBox.Text = userStorage.Password;
+
+            // 读取登陆设置放入CheckBox
+            var loginSettingsStorage = _storageService.GetLoginSettingsStorage();
+            RememberCheckBox.Checked = loginSettingsStorage.RemeberMe;
+            AutoLoginCheckBox.Checked = loginSettingsStorage.AutoLogin;
+        }
+
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <param name="username">用户名</param>
+        /// <param name="password">密码</param>
+        private void Login(string username, string password)
+        {
+            var result = _authApi.Login(username, password);
+            if (!result.IsSuccess())
+            {
+                DSkinMessageBox.Show(result.Message, "登陆失败", MessageBoxButtons.OK);
+            }
+            else
+            {
+                // 保存设置
+                SaveSettings(result.Entity.Token);
+                DialogResult = DialogResult.OK;
+
+                // 调用登陆成功方法
+                LoginAction?.Invoke(result.Entity.User);
+
+                Close();
+            }
+        }
+
+        private void SaveSettings(string token)
+        {
+            var loginSettingsStorage = new LoginSettingsStorage();
+
+            // 记住密码时，存储用户信息
+            if (RememberCheckBox.Checked)
+            {
+                loginSettingsStorage.RemeberMe = true;
+
+                var userStorage = new UserStorage();
+
+                var username = UsernameTextBox.Text;
+                userStorage.Username = username;
+
+                var password = PasswordTextBox.Text;
+                userStorage.Password = password;
+
+                _storageService.SaveStorage(userStorage);
+            }
+
+            // 自动登陆时，存储登陆设置
+            if (AutoLoginCheckBox.Checked)
+            {
+                loginSettingsStorage.AutoLogin = true;
+                loginSettingsStorage.Token = token;
+            }
+            _storageService.SaveStorage(loginSettingsStorage);
+        }
+
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            // 表单验证
             var username = UsernameTextBox.Text;
             if (username.Trim() == string.Empty)
             {
@@ -42,68 +113,22 @@ namespace SkyBlog
             Login(username,password);
         }
 
-        private void Login(string username,string password)
+        private void RememberCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            var result = _authApi.Login(username, password);
-            if (!result.IsSuccess())
+            // 如果取消'记住密码'，'自动登陆'也取消
+            if (!RememberCheckBox.Checked)
             {
-                DSkinMessageBox.Show(result.Message, "登陆失败", MessageBoxButtons.OK);
-            }
-            else
-            {
-                SaveSettings(result.Entity.Token);
-                DialogResult = DialogResult.OK;
-                LoginAction?.Invoke(result.Entity.User);
-
-                Close();
+                AutoLoginCheckBox.Checked = false;
             }
         }
 
-        private void LoginForm_Load(object sender, EventArgs e)
+        private void AutoLoginCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            var userStorage = _storageService.GetUserStorage();
-            UsernameTextBox.Text = userStorage.Username;
-            PasswordTextBox.Text = userStorage.Password;
-
-            var loginSettingsStorage = _storageService.GetLoginSettingsStorage();
-            RememberCheckBox.Checked = loginSettingsStorage.RemeberMe;
-            AutoLoginCheckBox.Checked = loginSettingsStorage.AutoLogin;
-        }
-
-        private void SaveSettings(string token)
-        {
-            var loginSettingsStorage = new LoginSettingsStorage();
-            if (RememberCheckBox.Checked)
-            {
-                loginSettingsStorage.RemeberMe = true;
-
-                var userStorage = new UserStorage();
-
-                var username = UsernameTextBox.Text;
-                userStorage.Username = username;
-
-                var password = PasswordTextBox.Text;
-                userStorage.Password = password;
-
-                _storageService.SaveStorage(userStorage);
-            }
-
+            // 如果启用'自动登陆'，'记住密码'也启用
             if (AutoLoginCheckBox.Checked)
             {
-                loginSettingsStorage.AutoLogin = true;
-                loginSettingsStorage.Token = token;
+                RememberCheckBox.Checked = true;
             }
-            _storageService.SaveStorage(loginSettingsStorage);
-        }
-
-        private void RememberCheckBox_Click(object sender, EventArgs e)
-        {
-            AutoLoginCheckBox.Checked = false;
-        }
-
-        private void AutoLoginCheckBox_Click(object sender, EventArgs e)
-        {
-            RememberCheckBox.Checked = true;
         }
     }
 }
