471f4f7bec240252d2d115f44fa0e9db8e54d793
zhuyst <13553371651@139.com>
2019-07-17 10:23:34 +0800

    refactor(client_web): 完成Api.ts重构

--- client_web_refactor/Constant.ts
+++ client_web_refactor/Constant.ts
@@ -1,2 +1,3 @@
-export const SKY_BLOG_URL = "https://github.com/zhuyst/SkyBlog";
-export const GITHUB_URL = "https://github.com/zhuyst";
+export const SKY_BLOG_URL: string = "https://github.com/zhuyst/SkyBlog";
+export const GITHUB_URL: string = "https://github.com/zhuyst";
+export const API_BASE_URL: string = "https://api.zhuyst.cc";
--- client_web_refactor/reducer/index.ts
+++ client_web_refactor/reducer/index.ts
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 
-export default combineReducers({});
+export default combineReducers({
+    form : formReducer,
+});
